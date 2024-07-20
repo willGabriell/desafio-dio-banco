25c72f86e4da25472a7aaae4171b52842d847f2b
Wilinton Gabriel <wilintongabrielss1@gmail.com>
2024-07-19 22:35:28 -0300

    Aula poo herança

--- src/ContaCorrente.java
+++ src/ContaCorrente.java
@@ -1,29 +1,3 @@
-public class ContaCorrente {
-    private int agencia;
-    private int numero;
-    private double saldo;
+public class ContaCorrente extends Conta{
 
-    public void sacar() {
-
-    }
-
-    public void depositar() {
-
-    }
-
-    public void transferir() {
-
-    }
-
-    public int getAgencia() {
-        return agencia;
-    }
-
-    public int getNumero() {
-        return numero;
-    }
-
-    public double getSaldo() {
-        return saldo;
-    }
 }
