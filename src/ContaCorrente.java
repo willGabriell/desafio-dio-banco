d01362f0c71ec15cf422072ceda8b786666251be
Wilinton Gabriel <wilintongabrielss1@gmail.com>
2024-07-19 22:16:11 -0300

    Aula poo abstação

---
+++ src/ContaCorrente.java
@@ -0,0 +1,17 @@
+public class ContaCorrente {
+    public int agencia;
+    public int numero;
+    public double saldo;
+
+    public void sacar() {
+
+    }
+
+    public void depositar() {
+
+    }
+
+    public void transferir() {
+
+    }
+}
